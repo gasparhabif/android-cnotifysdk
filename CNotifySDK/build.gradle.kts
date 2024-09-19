35eba12bab14b7c590e3b31c7a86529bae216496
Gaspi Habif <gasparhabif@gmail.com>
2024-09-18 22:26:45 -0300

    feat: publishing

--- CNotifySDK/build.gradle.kts
+++ CNotifySDK/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-    id("maven-publish")
+    `maven-publish`
 }
 
 android {
@@ -10,8 +10,9 @@ android {
 
     defaultConfig {
         minSdk = 24
-//        versionCode = 1
-//        versionName = "0.1.0"
+        aarMetadata {
+            minCompileSdk = 24
+        }
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
@@ -50,4 +51,52 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "me.cnotify"
+            artifactId = "cnotify_android_sdk"
+            version = "0.0.1"
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            pom {
+                name = "CNotifySDK"
+                description =
+                    "This SDK is used to connect the integration with cnotify.me. It provides functionality for handling push notifications and integrating with the cnotify.me service."
+                url = "https://cnotify.me"
+                properties = mapOf(
+                    "myProp" to "value",
+                    "prop.with.dots" to "anotherValue"
+                )
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "tlofano"
+                        name = "Tomás Lofano"
+                        email = "tomas@eruka.tech"
+                    }
+                    developer {
+                        id = "gaspihabif"
+                        name = "Gaspar Habif"
+                        email = "gaspar@eruka.tech"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/gasparhabif/android-cnotifysdk.git"
+                    developerConnection = "scm:git:ssh://github.com/gasparhabif/android-cnotifysdk.git"
+                    url = "https://cnotify.me"
+                }
+            }
+        }
+    }
 }
