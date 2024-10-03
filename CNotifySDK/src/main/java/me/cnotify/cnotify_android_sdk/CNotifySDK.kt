c69ee29b2fd7110b62fc7b92c9f188718fb93546
Tomás Lofano <tomilofano@gmail.com>
2024-10-03 20:03:38 -0300

    fix: credentials location

--- CNotifySDK/src/main/java/me/cnotify/cnotify_android_sdk/CNotifySDK.kt
+++ CNotifySDK/src/main/java/me/cnotify/cnotify_android_sdk/CNotifySDK.kt
@@ -40,7 +40,7 @@ class CNotifySDK private constructor(
     }
 
     private fun initializeFirebase() {
-        printCNotifySDK("🚀 Initializing (Version: 0.3.4)")
+        printCNotifySDK("🚀 Initializing (Version: 0.3.5)")
         if (FirebaseApp.getApps(getContext()).isEmpty()) {
             printCNotifySDK("⚙️ Configuring Firebase app")
             FirebaseApp.initializeApp(getContext(), getFirebaseOptions())
